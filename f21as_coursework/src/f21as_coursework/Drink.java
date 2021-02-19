20a4026ee43f156ab483d5fdf605dde0f6b67302
islagunn <islagunn@islas-air>
2021-02-19 14:55:20 +0000

    item classes

---
+++ f21as_coursework/src/f21as_coursework/Drink.java
@@ -0,0 +1,35 @@
+package f21as_coursework;
+
+
+public class Drink extends Items {
+	
+	//instance variables
+	private String size; 		//size of drink - small, regular, large
+	private String type;		//type of drink - hot, iced, juice
+	private String flavour;		//flavour of drink - vanilla, caramel
+	
+	//constructor
+	public Drink (String cat,String id, String des, double cost, String size, String type, String flav) {
+		super (id, des, cat, cost);
+		this.size = size;
+		this.type = type;
+		flavour = flav;
+	}
+	
+	//return size
+	public String getSize() {
+		return size;
+	}
+	
+	//return type
+	public String getType() {
+		return type;
+	}
+	
+	//return flavour
+	public String getFlavour() {
+		return flavour;
+	}
+	
+
+}
---
+++ f21as_coursework/src/f21as_coursework/ItemList.java
@@ -0,0 +1,19 @@
+package f21as_coursework;
+
+import java.util.HashMap;
+import f21as_coursework.Items;
+
+public class ItemList {
+
+	public static void main(String[] args) {
+		//create HashMap object called items 
+		//where key = identifier (String) and value = menu item (String)
+		HashMap<String, Items> items = new HashMap<String, Items>();
+		
+
+		/*
+		 * finishing off HashMap 
+		 */
+		
+	}
+}
