c6d346526f6bac3bba073594d6ef6b506ece2b11
ig019 <71330859+ig019@users.noreply.github.com>
2021-02-19 20:41:29 +0000

    Update ItemList.java

--- f21as_coursework/src/f21as_coursework/ItemList.java
+++ f21as_coursework/src/f21as_coursework/ItemList.java
@@ -10,6 +10,23 @@ public class ItemList {
 		//where key = identifier (String) and value = menu item (String)
 		HashMap<String, Items> items = new HashMap<String, Items>();
 		
+		//print items in map
+		System.out.println(items);
+		System.out.println();
+		
+		//print size of map
+		System.out.println("Size of map is " + items.size());
+		
+		//loop through HashMap to print keys (identifiers)
+		for (String iden : items.keySet()) {
+			System.out.println(iden);
+		}
+		
+		///print keys and  values
+		for (String h : items.keySet()) {
+			System.out.println("key: " + h + items.get(h));
+		}
+
 
 		/*
 		 * finishing off HashMap 
